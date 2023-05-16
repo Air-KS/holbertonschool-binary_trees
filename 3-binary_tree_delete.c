99f84cea820208af8d7e79c4ac6c8408c9ab5931
lenne sebastien <sebastienlenne@gmail.com>
2023-05-16 13:22:39 +0200

    update file task 3 error compilation

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,14 +3,14 @@
 /**
  * binary_tree_delete - function that delete a node
  * @tree: tree structur
- * 
+ *
 */
 void binary_tree_delete(binary_tree_t *tree)
 {
-        if (tree == NULL)
-                return;
+	if (tree == NULL)
+		return;
 
-                binary_tree_delete(tree->left);
-                binary_tree_delete(tree->right);
-                free(tree);
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
 }
