43eb2d646f21c576c6e20238e60bb3688f7c85f0
lenne sebastien <sebastienlenne@gmail.com>
2023-05-16 13:41:14 +0200

    update file task 4 error betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_leaf - function that checks if a node is a leaf
- * @node: node structur
- * return: integer
+ * binary_tree_is_leaf - checks if a node is a leaf.
+ * @node: Pointer to the node to check.
+ *
+ * Return: 1 if the node is a leaf, 0 otherwise.
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
