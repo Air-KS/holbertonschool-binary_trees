fd0e3c5a04d996342f591ac23fcfbc522620fa01
lenne sebastien <sebastienlenne@gmail.com>
2023-05-17 11:03:57 +0200

    add file task 14 verification checkeur

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the height
+ * Return: 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+/*+1 pour obtenir la hauteur de l'arbre de manière récursive*/
+	left_height = binary_tree_height(tree->left) + 1;
+	right_height = binary_tree_height(tree->right) + 1;
+	return ((left_height > right_height) ? left_height : right_height);
+}
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the balance factor
+ * Return: 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (0);
+}
